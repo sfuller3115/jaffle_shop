8de0606a9b2cab23ec8cc246eadb34ba673fc366
stevefuller3115@gmail.com <stevefuller3115@gmail.com>
2025-12-16 05:48:38 +0000

    upfdf

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,7 +2,12 @@ with
 
     source as (select * from {{ source("jaffle_shop", "orders") }}),
 
-    renamed as (select id, user_id, order_date, status, _etl_loaded_at from source)
+    renamed as (select id as order_id, 
+                       user_id as customer_id, 
+                       order_date, 
+                       status as order_status, 
+                       _etl_loaded_at 
+                from source)
 
 select *
 from renamed
